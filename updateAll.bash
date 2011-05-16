b09dc024f57a90a33436620a066468b5c7cfbb0a
adamfisk <a@littleshoot.org>
2011-05-16 16:52:10 -0700

    New update script

---
+++ updateAll.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+function die() {
+  echo $*
+  exit 1
+}
+
+DIRS=`ls`
+for x in $DIRS
+do
+  test -f $x && continue
+  echo $x | grep target && continue 
+  echo $x | grep ".git" && continue 
+  cd $x
+  git pull origin
+  cd -
+done
