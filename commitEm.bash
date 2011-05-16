cff4e6efa807c1c21b1d631e8974993d61fa0805
adamfisk <a@littleshoot.org>
2011-05-16 13:21:36 -0700

    Added args check and comment

--- commitEm.bash
+++ commitEm.bash
@@ -5,11 +5,20 @@ function die() {
   exit 1
 }
 
+ARGS=1  # One arg to script expected.
+
+if [ $# -ne "$ARGS" ]
+then
+    die "$0: Received $# args...commit comment required"
+fi
+
+comment=$1
+
 DIRS=`ls`
 for x in $DIRS
 do
   test -f $x && continue
   cd $x
-  git add .gitignore;git commit -a -m "Added common maven parent";git push origin master
+  git add .gitignore;git commit -a -m "$comment";git push origin master
   cd -
 done
