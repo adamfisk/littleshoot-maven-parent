c15188a7244813feb0ccf7b111a2da258a4be00c
Adam Fisk <a@littleshoot.org>
2013-01-14 19:05:06 -0800

    Version updates and script readability cleanups

--- allGit.bash
+++ allGit.bash
@@ -5,6 +5,9 @@ function die() {
   exit 1
 }
 
+#red='\e[0;31m'
+#NC='\e[0m' # No Color
+
 searchTerms=$1
 DIRS=`ls`
 #echo "Searching jars $JARS"
@@ -12,6 +15,7 @@ for x in $DIRS
 do
   test -f $x && continue
   cd $x
-  git status
-  cd -
+  echo "Checking git status for $x"
+  git status | grep -v "Changes not staged for commit" | grep -v "On branch master" | grep -v "nothing added to commit but untracked files present" | grep -v "to update what will be committed" | grep -v "to discard changes in working directory" | grep -v "include in what will be committed" | grep -v "Untracked files" | grep -v "working directory clean" | grep -v "to unstage" | grep -v "Changes to be committed" 
+  cd - &> /dev/null
 done
